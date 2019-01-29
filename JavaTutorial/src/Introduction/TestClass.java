a7225645a3421c3cfd919cd3795a0b6eeac22b3f
Elizabeth Perez <elizabeth.perez@LEON-LT-181010E.ad.unosquare.com>
2019-01-29 16:35:27 -0600

    First Commit

---
+++ JavaTutorial/src/Introduction/TestClass.java
@@ -0,0 +1,14 @@
+package Introduction;
+
+public class TestClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// First program
+		/*
+		 * 1 test
+		 */
+System.out.println("Hello world");
+	}
+
+}
